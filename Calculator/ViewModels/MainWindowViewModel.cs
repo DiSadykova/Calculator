233760307795540e0d9c64f61039ba8d8737c2c3
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2022-01-26 21:31:52 +0300

    Добавьте файлы проекта.

---
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanfed([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double var;
+        public double Var
+        {
+            get => var;
+            set
+            {
+                var = value;
+            }
+        }
+        public ICommand AddCommand {get;}
+        private void OnAddCommandExecute(object p)
+        {
+            
+        }
+
+    }
+}
