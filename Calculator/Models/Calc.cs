ad563fb1128dc8227ce44379a89369b581ebbc05
Ди <Dilyara.mozhet.sadykova@gmail.com>
2022-01-27 16:42:01 +0300

    3

--- Calculator/Models/Calc.cs
+++ Calculator/Models/Calc.cs
@@ -8,5 +8,45 @@ namespace Calculator.Models
 {
     internal class Calc
     {
+        public static double Calcul(string exp)
+        {
+            char[] array = exp.ToCharArray();
+            List<char> charList = new List<char>();
+            charList.AddRange(array);
+            List<string> priority1 = new List<string>();
+            Stack<int> g = new Stack<int>();
+            string priorityStr ="";
+
+
+            for (int i = 0; i < charList.Count; i++)
+            {
+                switch (charList[i])
+                {
+                    case '(':
+                        
+                        g.Push(i);
+                        break;
+                    case ')':
+                        if (g.Count!=0)
+                        {
+                            for (int j = g.Pop(); j < i-1; j++)
+                            {
+                                priorityStr+=charList[j];
+                            }
+                            priority1.Add(priorityStr);
+                            break;
+                        }
+                        else
+
+
+                        
+                }
+            }
+            {
+                double result=0;
+                return result;
+            }
+        }
+
     }
 }
